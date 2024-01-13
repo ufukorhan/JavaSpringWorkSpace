8a20bc5e00b4340b19f54afe1990a4fe5e18d142
Ufuk Orhan <ufukorhan@Ufuks-MBP.home>
2024-01-13 13:01:00 +0300

    Regions and monsters added

--- adventure-game/src/Game.java
+++ adventure-game/src/Game.java
@@ -14,17 +14,20 @@ public class Game {
 
         Location location = null;
 
-        while (true){
+        while (true) {
             player.printInfo();
             System.out.println();
             System.out.println("########### Bölgeler ###########");
             System.out.println();
             System.out.println("1 - Güvenli Ev --> Burada düşman yok güvenli bir ev");
-            System.out.println("2 - Mağaza --> Silah veya Zırh satın alabilirsiniz !");
+            System.out.println("2 - Eşya Dükkanı --> Silah veya Zırh satın alabilirsiniz !");
+            System.out.println("3 - Mağara --> Ödül <Yemek>, dikkatli ol karşına zombi çıkabilir !");
+            System.out.println("4 - Orman --> Ödül <Odun>, dikkatli ol karşına vampir çıkabilir !");
+            System.out.println("5 - Nehir --> Ödül <Su>, dikkatli ol karşına ayı çıkabilir !");
             System.out.println("0 - Çıkış Yap --> Oyunu sonlandır.");
             System.out.print("Lütfen gitmek istediğiniz bölgeyi seçiniz : ");
             int selectLoc = input.nextInt();
-            switch (selectLoc){
+            switch (selectLoc) {
                 case 0:
                     location = null;
                     break;
@@ -34,14 +37,23 @@ public class Game {
                 case 2:
                     location = new ToolStore(player);
                     break;
+                case 3:
+                    location = new Cave(player);
+                    break;
+                case 4:
+                    location = new Forest(player);
+                    break;
+                case 5:
+                    location = new River(player);
+                    break;
                 default:
-                    location = new SafeHouse(player);
+                    System.out.println("Lütfen geçerli bir bölge giriniz !");
             }
-            if (location == null){
+            if (location == null) {
                 System.out.println("Oyun sonlandırıldı !");
                 break;
             }
-            if (!location.onLocation()){
+            if (!location.onLocation()) {
                 System.out.println("GAME OVER !");
                 break;
             }
